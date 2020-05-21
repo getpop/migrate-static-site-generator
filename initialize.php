83ae19eb8025f7152971218885acf52baa2993bf
Leonardo Losoviz <leo@getpop.org>
2020-05-21 19:48:39 +0800

    Added type "void" to functions

--- initialize.php
+++ initialize.php
@@ -11,12 +11,12 @@ $class = (new class() extends \PoP\Root\Component\AbstractComponent
      *
      * @return void
      */
-    public static function beforeBoot()
+    public static function beforeBoot(): void
     {
         parent::beforeBoot();
 
         // Initialize code
-        require_once 'migrate/pop-static-site-generator.php';  
+        require_once 'migrate/pop-static-site-generator.php';
     }
 });
 $class::initialize();
